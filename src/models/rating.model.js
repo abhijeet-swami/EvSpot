02a329f5113dde470de29856abad04bf1c83167b
Abhijeet Swami <swami2005abhijeet@gmail.com>
2025-09-08 10:08:40 +0530

    feat: add rating schema

---
+++ src/models/rating.model.js
@@ -0,0 +1,29 @@
+import mongoose from "mongoose";
+
+const ratingSchema = new mongoose.Schema(
+  {
+    rating: {
+      type: Number,
+      required: true,
+      min: 1,
+      max: 5,
+    },
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    station: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Station",
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
+
+ratingSchema.index({ user: 1, station: 1 }, { unique: true });
+
+export const Rating = mongoose.model("Rating", ratingSchema);
