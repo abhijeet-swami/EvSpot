2b6951dc8071bc3cbec86fb4ae0bd3d44074491d
Abhijeet Swami <swami2005abhijeet@gmail.com>
2025-09-08 23:30:00 +0530

    feat: add cookie-parser

--- src/app.js
+++ src/app.js
@@ -1,8 +1,10 @@
 import express from "express";
+import cookieParser from "cookie-parser";
 
 const app = express();
 
 app.use(express.json());
+app.use(cookieParser());
 
 import authRouter from "./routes/auth.route.js";
 app.use("/api/v1/auth", authRouter);
--- src/utils/sendResponse.util.js
+++ src/utils/sendResponse.util.js
@@ -1,4 +1,13 @@
-function sendResponse(res, statusCode, message, data = null) {
+function sendResponse(res, statusCode, message, data = null, token = null) {
+  if (token) {
+    res.cookie("uid", token, {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === "production",
+      sameSite: "strict",
+      maxAge: 7 * 24 * 60 * 60 * 1000,
+    });
+  }
+
   return res.status(statusCode).json({
     sucess: true,
     message,
