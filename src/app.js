986ab0b1b03cd63200722b4e58e7a90986724820
Abhijeet Swami <swami2005abhijeet@gmail.com>
2025-09-09 16:03:17 +0530

    feat: add user route

--- src/app.js
+++ src/app.js
@@ -9,4 +9,7 @@ app.use(cookieParser());
 import authRouter from "./routes/auth.route.js";
 app.use("/api/v1/auth", authRouter);
 
+import userRouter from "./routes/user.route.js";
+app.use("/api/v1/user", userRouter);
+
 export default app;
---
+++ src/routes/user.route.js
@@ -0,0 +1,10 @@
+import express from "express";
+
+const router = express.Router();
+
+import { verifyAuth } from "../middlewares/auth.middleware.js";
+import { fetchUserProfile } from "../controllers/user.controller.js";
+
+router.get("/me", verifyAuth, fetchUserProfile);
+
+export default router;
