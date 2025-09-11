8fb8580b77d7cd97ee2ed6c8db7b097ef62f101c
Abhijeet Swami <swami2005abhijeet@gmail.com>
2025-09-11 09:13:56 +0530

    feat: add comment route

--- src/routes/station.route.js
+++ src/routes/station.route.js
@@ -1,4 +1,5 @@
 import express from "express";
+import { verifyAuth } from "../middlewares/auth.middleware.js";
 
 const router = express.Router();
 
@@ -8,5 +9,12 @@ import {
 } from "../controllers/station.controller.js";
 router.get("/nearby", nearbyStation);
 router.get("/", getStationByCity);
+
+import {
+  addComment,
+  deleteComment,
+} from "../controllers/comment.controller.js";
+router.post("/addcomment", verifyAuth, addComment);
+router.delete("/deletecomment", verifyAuth, deleteComment);
 
 export default router;
